b8cd4ba0c5a9bd686829fa6d7cb31e37cb6122ec
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2025-07-06 18:09:36 +0530

    login endpoint created

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { pingTest, fetchUsers, addUser } from '../controllers/userController.js'
+import { login } from '../controllers/authController.js';
 
 const router = express.Router()
 
 router.get('/ping', pingTest);
 router.get('/getusers', fetchUsers);
 router.post('/adduser', addUser)
-
+router.post('/login', login);
 export default router;
