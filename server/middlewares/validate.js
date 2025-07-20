eb02ccfbc4ad5acc14f8affce5724bbfbcec9e38
PIPALIYAJAYDIP <pipaliyaj3@gmail.com>
2025-07-21 01:18:45 +0530

    schema validation added on login and register route

---
+++ server/middlewares/validate.js
@@ -0,0 +1,19 @@
+export const validate = (schema, property = 'body') => {
+    return (req, res, next) => {
+        const { error } = schema.validate(req[property], { abortEarly: false });
+
+        if (error) {
+            const messages = error.details.map((details) => {
+                return details.message;
+            })
+            return next({
+                statusCode: 400,
+                errorCode: "SCHEMA_VALIDATION_FAILED",
+                message: messages,
+            });
+        }
+
+        next();
+
+    }
+}
